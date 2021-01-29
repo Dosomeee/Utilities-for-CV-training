3c2999e721a77a3ba85c12d1efadd3305baeb636
Xunjie <634629477@qq.com>
2021-01-29 18:18:40 +0800

    Finish download function, need to add shape to json

---
+++ Downloader.py
@@ -0,0 +1,92 @@
+import pickle
+from main import pretty_print
+import os
+import aiohttp
+import asyncio
+import aiofiles
+import json
+import cv2
+
+
+async def download_image(url, image_path):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(image_path, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+
+
+# k = ./results/小齐/
+# v = ./results/小齐/xxx.json
+def get_json_path_list(input_path):
+    result = dict()
+    for directory in os.listdir(input_path):
+        abs_path = os.path.join(input_path, directory)
+        for i in os.listdir(abs_path):
+            dataset_path = os.path.join(abs_path, i)
+            for f in os.listdir(dataset_path):
+                if f.split('.')[-1] == 'json':
+                    result[dataset_path] = os.path.join(dataset_path, f)
+
+    return result
+
+
+# k = ./results/小齐/无锡电梯超员_24899_2021_01_04/20210104114539.jpg
+# v = url
+def get_path_url_dict(input_dict):
+    result = dict()
+    for k, v in input_dict.items():
+        with open(v) as json_file:
+            j = json.load(json_file)
+            for i in j['images']:
+                filename = i['filename']
+                image_url = i['url']
+
+                result[os.path.join(k, filename)] = image_url
+
+    return result
+
+
+def update_json(json_path, filename, shape):
+    with open(json_path) as json_file:
+        j = json.load(json_file)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    Min = ['+86 15137393991', '123456']
+    Qing = ['+86 15536902280', 'cike567']
+
+    result_path = './results'
+    username = Min[0]
+    pwd = Min[1]
+
+    # k = ./results/小齐/
+    # v = ./results/小齐/无锡电梯超员_24899_2021_01_04/xxx.json
+    json_path_dict = get_json_path_list(result_path)
+    imagePath_imageURL_dict = get_path_url_dict(json_path_dict)
+
+    # k = ./results/小齐/无锡电梯超员_24899_2021_01_04/20210104114539.jpg
+    # v = url
+    current_json_path = None
+    current_directory = None
+    update_list = []
+    for k, v in imagePath_imageURL_dict.items():
+        # update current_directory and current_json_dict when changing directory
+        if (not current_json_path) or (current_directory != os.path.dirname(k)):
+            current_directory = os.path.dirname(k)
+            # i = ./results/小齐/无锡电梯超员_24899_2021_01_04/xxx.json
+            for i in json_path_dict.values():
+                if os.path.dirname(i) == current_directory:
+                    current_json_path = i
+
+        loop.run_until_complete(download_image(v, k))
+        shape = cv2.imread(k, 0).shape
+
+        update_info = {
+            'json_path':
+        }
+
+        update_json(current_json_path, k.split('/')[-1], shape)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import json
 import os
 import aiofiles
 import cv2
+import pickle
 
 # 1. Login and get a user token
 # 2. Use this user token, to get a uuid
@@ -38,7 +39,11 @@ global loop
 async def post(url, payload=None):
     async with aiohttp.ClientSession() as session:
         async with session.post(url, data=payload) as response:
-            print("Status:", response.status)
+            if response.status != 200:
+                print("Status:", response.status)
+                print("Request: ", response.url)
+            else:
+                print("Success: ", response.url)
             result_json = await response.json()
             return result_json['data']['access_token']
 
@@ -46,7 +51,11 @@ async def post(url, payload=None):
 async def get(url, payload=None, header=None):
     async with aiohttp.ClientSession() as session:
         async with session.get(url, params=payload, headers=header) as response:
-            print("Status:", response.status)
+            if response.status != 200:
+                print("Status:", response.status)
+                print("Request: ", response.url)
+            else:
+                print("Success: ", response.url)
             result_json = await response.json()
             return result_json
 
@@ -58,10 +67,12 @@ def pretty_print(input_json):
     print(json.dumps(parsed, indent=4, sort_keys=True))
 
 
-def get_taskUUID_datasetUUI_taskStatus(task_json):
+def get_task_details(task_json):
     tasks = dict()
     for task in task_json['data']['items']:
-        tasks[task['uuid']] = {'dataset_uuid': task['dataset_uuid'], 'status': task['status']}
+        tasks[task['uuid']] = {'dataset_uuid': task['dataset_uuid'], 'status': task['status'],
+                               'task_name': task['meta']['taskName'], 'task_username': task['meta']['taskUserName'],
+                               'executor_username': task['executor']['username']}
 
     return tasks
 
@@ -102,12 +113,28 @@ def get_user_info(username, password):
     return user_uuid, user_token
 
 
+async def get_nickname(uuid, user_token):
+    url = 'https://aist.feieee.com/api/user/' + uuid
+    headers = {'Authorization': 'Bearer ' + user_token}
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers) as response:
+            if response.status != 200:
+                print("Status:", response.status)
+                print("Request: ", response.url)
+            else:
+                print("Success: ", response.url)
+
+            result_json = await response.json()
+            return result_json['data']['username']
+
+
 def get_task_info(user_uuid, user_token):
     # Get task list
     params = {'page_size': 1000, 'page_index': 1}
     headers = {'Authorization': 'Bearer ' + user_token}
     get_task_url = 'https://aist.feieee.com/api/user/' + user_uuid + '/published_task'
     task_json = loop.run_until_complete(get(get_task_url, params, headers))
+    # pretty_print(task_json)
 
     return task_json
 
@@ -135,13 +162,17 @@ def download_dataset(username, password):
     task_json = get_task_info(user_uuid, user_token)
 
     # Get dataset list and their status
-    tasks = get_taskUUID_datasetUUI_taskStatus(task_json)
+    tasks = get_task_details(task_json)
+    # print(tasks)
 
-    # one item in tasks: {task_uuid: {task_status, dataset_uuid}}
+    accept_status = ['COMMITTED', 'ARCHIVED']
+    datasets = dict()
+
     for k, v in tasks.items():
-        if v['status'] == 'COMMITTED':
+        if v['status'] in accept_status:
             dataset_list = get_dataset_list(user_token, v['dataset_uuid'])
             label_category_list = dataset_list['data']['self']['label_category']
+            label_type = dataset_list['data']['self']['label_type']
             # pretty_print(dataset_list)
 
             # image uuid and its url, label
@@ -156,30 +187,52 @@ def download_dataset(username, password):
                 images.append(image)
 
             result = {
+                "label_type": label_type,
                 "images": images,
                 "label_category": label_category_list
             }
 
-            name = 'result' + '-' + datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".json"
-            download_json(result, path=r'/home/xunjie/PycharmProjects/TransferDataset/results/', name=name)
+            datasets[v['task_name']] = {'result': result, 'task_username': v['task_username'],
+                                        'executor_username': v['executor_username']}
+
+    return datasets
 
 
-async def download_image(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                f = await aiofiles.open('./results/test.jpg', mode='wb')
-                await f.write(await resp.read())
-                await f.close()
+def create_dataset_folder(input_name, input_path):
+    new_path = os.path.join(input_path, input_name)
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+        print('New path created: ', new_path)
+        return new_path
+    else:
+        print("Already exists")
+        return new_path
+
+
+def save_to_folder(datasets, nickname, path):
+    for task_name, data in datasets.items():
+        name = task_name + ".json"
+        dataset_folder_path = create_dataset_folder(task_name, create_dataset_folder(nickname, path))
+        if dataset_folder_path:
+            download_json(data['result'], path=dataset_folder_path, name=name)
+        else:
+            print("Already downloaded")
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    # url = "http://data.vsais.com/197ec0f6-0bd8-42bc-8c9c-29fd4f956c5e/20210121031628318933902.jpg?e=1611839106&token=kMwTtMsmCVtbGgI-l9gw1de9fmoaDO4nj67cFw3l:MnKRds01pyXa5p7Of6dp741Q6cs="
-    # loop.run_until_complete(download_image(url))
-    image = cv2.imread('./results/test.jpg', 0)
-    # (width, high)
-    print(image.shape)
 
-    # download_dataset(username='+86 15536902280', password='cike567')
-    # download_image('http://data.vsais.com/197ec0f6-0bd8-42bc-8c9c-29fd4f956c5e/20210121031628318933902.jpg?e=1611839106&token=kMwTtMsmCVtbGgI-l9gw1de9fmoaDO4nj67cFw3l:MnKRds01pyXa5p7Of6dp741Q6cs=')
+    Min = ['+86 15137393991', '123456']
+    Qing = ['+86 15536902280', 'cike567']
+
+    result_path = './results'
+    username = Min[0]
+    pwd = Min[1]
+
+    uuid, token = get_user_info(username=username, password=pwd)
+    nickname = loop.run_until_complete(get_nickname(uuid, token))
+
+    # for published tasks which are "COMMITTED' and "ACHIEVED"
+    datasets = download_dataset(username=username, password=pwd)
+    save_to_folder(datasets, nickname, result_path)
+
